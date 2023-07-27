ca0d2262c3db7388e269651c3559dee20b37eeed
Md. Hafijur Rahman <hafijur@bdtask.net>
2023-07-27 15:23:00 +0600

    notification sent

--- events/message_sent.action.js
+++ events/message_sent.action.js
@@ -2,6 +2,7 @@ const { io } = require("../app");
 const dbService = require("../service/db.service");
 const Notification = require("../service/notification.service");
 const tag = require("../constants/event.constants");
+const appConfig = require("../config/app.config");
 
 /**
  * handles message sending action
@@ -16,115 +17,106 @@ const tag = require("../constants/event.constants");
  * }} payload
  */
 
-async function MessageSent(payload) {
+async function MessageSentGp(payload) {
+  // console.log(`\nMessageSent Payload ----`, payload);
   try {
-    const { text, sender_id, receiver_id, sender_name, sender_image, room_id } = payload;
+    const { text, activity_id, sender_id, chat_message_type } = payload;
 
-    // console.log(`\nMessageSent Payload ----${JSON.stringify(payload)}`);
-
-    if (sender_id === receiver_id) {
-      // console.log(`\nsender_id and receiver_id are same--- sender_id: ${sender_id}, receiver_id: ${receiver_id}`);
-      return;
-    }
+    // console.log(`\nMessageSent Payload ----${payload}`);
 
     const newMessage = await dbService
-      .table("jp_single_chat_message")
+      .table("jp_group_chat_message")
       .insert({
         text,
-        sender_id,
-        receiver_id,
         sent_at: Date.now(),
-        received_at: 0,
+        activity_id,
+        sender_id,
         status: "sent",
-        room_id: room_id || "",
+        chat_message_type,
+        owner: "",
       });
-      // .returning("*");
 
-    // console.log({ newMessage });
+    const activityInfo = await dbService.select(["*"]).from("jp_activity").where({ activity_id });
+    const activityAttendant = await dbService.select(["user_id"]).from("jp_activity_attendant").where({ activity_id });
 
-    // const foundUser = await dbService.raw(`
-    //   SELECT 
-    //     juo.device_token, socket_id, online_status, juo.profile_picture, juo.profile_name
-    //   FROM
-    //     jp_user_online juo
-    //   INNER JOIN
-    //     users jbu
-    //   ON
-    //     jbu.id = juo.user_id
-    //   AND
-    //     jbu.account_status = 'active'
-
-    //   WHERE
-    //     juo.user_id = ${receiver_id}
-    // `);
-    const foundUser = await dbService.select(["*"])
-      .from("jp_user_online")
-      .innerJoin("users", "users.id", "jp_user_online.user_id")
-      .where({ "jp_user_online.user_id": receiver_id, "users.account_status": "active" });
-
-
-    // console.log("Message sent -> found user", foundUser);
-
-    const receiverInfo = foundUser[0];
-
-    // console.log("reciever info", receiverInfo);
-
-    if (!Object.keys(receiverInfo).length) {
-      console.log('no receiver info');
+    console.log('attendantList', activityAttendant);
+    if (!Object.keys(activityInfo).length) {
       return;
     }
 
-    // if (receiverInfo.send_notification) {
-    //   const notificationService = new Notification();
+    activityAttendant.forEach(async (attendant) => {
+      const senderInfo = await dbService.select(["socket_id","user_id","device_token"])
+        .from("jp_user_online")
+        .where({ user_id: attendant.user_id });
+      console.log("Sender info", senderInfo);
 
-    //   const notificationPayload = {
-    //     title: `${sender_name} sent you a message`,
-    //     body: text,
-    //     type: "chat_message",
-    //     fcm_token: receiverInfo.device_token,
-    //     sender_image,
-    //     sender_name,
-    //     sender_id,
-    //     multiple: false,
-    //     chat_message_type: "text",
-    //     message_id: newMessage[0].single_message_id,
-    //   };
+      const baseUser = await dbService.select("*").from("users").where({ id: sender_id });
+      // console.log('base user', baseUser);
+      if (activity_id) {
+        // eslint-disable-next-line eqeqeq
+        if (sender_id == attendant.user_id) {
+          // Thie user con't send any  notification
+        } else {
+          const notificationService = new Notification();
 
-    //   notificationService.send(notificationPayload);
-    // }
+          const notificationPayload = {
+            title: `${baseUser[0].name}`,
+            body: text,
+            type: "chat_message",
+            activity_id,
+            fcm_token: senderInfo[0].device_token,
+            // fcm_token: "e6ZWuAylRASM-FcCUPSuJy:APA91bFQe-3nE7tkGf7VOaJfGJg41e2Z8vZp7Uv5fw_tGqBhyZAJmqfO_qNFakV2rI2CvPBoxKdWHv31deGJb0pYVFolpwsxhlhDfIb16Kky3uJ2KVCTqsZ7qAfj-YawNtXjqwIHHQMS", //senderInfo[0].device_token,
+            sender_image: (baseUser[0].profile_photo_path == null) ? null : `${appConfig.PHOTO_BASE_PATH}/${baseUser[0].profile_photo_path}`,
+            sender_name: baseUser[0].username,
+            sender_id,
+            multiple: false,
+            chat_message_type: "text",
+            message_id: newMessage[0].single_message_id,
+          };
 
-    const sockets = [];
-    console.log('recieverInfo socket', receiverInfo.socket_id);
-    if (receiverInfo.socket_id) {
-      sockets.push(receiverInfo.socket_id);
-      console.log('inside sockets', sockets);
-      io.to(sockets).emit(tag.GET_MESSAGE, payload);
-    }
-    // console.log({ sockets });
+          // console.log('working-----------------',notificationPayload);
 
-    const recentMessagePayload = {
-      ...newMessage[0],
-      sender_name,
-      sender_image,
-      sender_id,
-      receiver_id,
-      receiver_name: receiverInfo.profile_name,
-      receiver_image: receiverInfo.profile_picture,
-      chat_type: "single",
-    };
+          notificationService.send(notificationPayload);
+        }
+      }
 
-    const senderInfo = await dbService.select(["socket_id"]).from("jp_user_online").where({ user_id: sender_id });
+      const sockets = [];
+      if (senderInfo[0].socket_id) {
+        // find user_id from senderifo where user_id != sender_id
+        senderInfo.forEach((info) => {
+          // if (info.user_id !== sender_id) {
+          sockets.push(info.socket_id);
+          // }
+        });
 
-    if (senderInfo.length && senderInfo[0].socket_id) {
-      sockets.push(senderInfo[0].socket_id);
-    }
+        sockets.push(senderInfo[0].socket_id);
+        console.log('payload');
+        console.log(payload);
+        console.log('sockets found', sockets);
+        io.to(sockets).emit(tag.GET_MESSAGE_GP, payload);
+      }
 
-    // console.log({ sockets });
+      // // console.log({ sockets });
+      const recentMessagePayload = {
+        ...newMessage[0],
+        sender_name: senderInfo[0].username,
+        sender_image: senderInfo[0].profile_picture,
+        sender_id,
+        activity_id,
+        chat_type: "group",
+      };
 
-    io.to(sockets).emit(tag.RECENT_CHAT, recentMessagePayload);
+      if (senderInfo.length && senderInfo[0].socket_id) {
+        sockets.push(senderInfo[0].socket_id);
+      }
+
+      // // console.log({ sockets });
+
+      io.to(sockets).emit(tag.RECENT_CHAT, recentMessagePayload);
+    });
   } catch (error) {
     console.log("\n\nFailed to sent message\n\n", error);
   }
 }
 
-module.exports = MessageSent;
+module.exports = MessageSentGp;
